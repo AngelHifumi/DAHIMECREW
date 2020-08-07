f3363afa24c63e06bf5868a46bfaa7d6e84fadc7
Marie Hifumi Starlight <42872306+AngelHifumi@users.noreply.github.com>
2020-08-07 14:00:34 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+
+const Discord = require("discord.js");
+const osu = require("node-osu");
+const client = new Discord.Client();
+client.login("NTE3Mzg4Mzk3Mzg0NDMzNjc0.W_7Nfg.n95IusaqYnxqbmxVCaKH6nSDZdk");
+
+client.on("ready", () => {
+	console.log("sup dog");
+});
+
+
+client.on("message", message => {
+    if (message.content.startsWith("*avatar")) {
+    if (!message.mentions.users.size) {
+        return message.channel.send(message.author.displayAvatarURL());
+    }
+
+    const avatarList = message.mentions.users.map(user => {
+        return user.displayAvatarURL();
+    });
+    message.channel.send(avatarList);
+    }
+    });
+
+client.on("message", message => {
+    if (message.content.startsWith("Praise Hime")){
+        message.channel.send("All Hail Hime");
+    }
+});
+
+
